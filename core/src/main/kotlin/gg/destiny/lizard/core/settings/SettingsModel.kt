8ec480e88e5f61a2b04d90bf6148587cb3d56587
Hilal Alsibai <xiphirx@users.noreply.github.com>
2018-06-16 23:49:15 -0700

    Introduce SettingSpec
    This is a generic and platiform agnostic way to represent a setting.
    
    The dark mode setting is introduced here to start with.

---
+++ core/src/main/kotlin/gg/destiny/lizard/core/settings/SettingsModel.kt
@@ -0,0 +1,17 @@
+package gg.destiny.lizard.core.settings
+
+interface SettingSpec<T> {
+  var value: T
+  val key: String
+  val defaultValue: T
+}
+
+enum class BooleanSetting : SettingSpec<Boolean> {
+  DARK_MODE {
+    override val key = "dark_mode"
+    override val defaultValue = true
+    override var value = defaultValue
+  }
+}
+
+data class SettingsModel(val settings: List<SettingSpec<out Any>> = emptyList())
