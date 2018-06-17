b90d68f56a028dfb30b1e69212e917099d982d8d
Hilal Alsibai <xiphirx@users.noreply.github.com>
2018-06-16 20:16:42 -0700

    Remove poor api extension

--- app/src/main/kotlin/gg/destiny/lizard/base/extensions/NumberExtensions.kt
+++ app/src/main/kotlin/gg/destiny/lizard/base/extensions/NumberExtensions.kt
@@ -2,10 +2,7 @@ package gg.destiny.lizard.base.extensions
 
 import android.content.Context
 import android.util.TypedValue
-import gg.destiny.lizard.App
 
 fun Int.dp(context: Context) =
   Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics))
 
-val Int.dp: Int
-  get() = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), App.INSTANCE.resources.displayMetrics))
