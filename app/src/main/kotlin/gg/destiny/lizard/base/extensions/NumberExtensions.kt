69df27ddbdd53dd0fa23d32defcf8756b63f73d4
Hilal Alsibai <xiphirx@users.noreply.github.com>
2018-10-12 22:59:35 -0700

    Add receiver annotation type to extension dp function

--- app/src/main/kotlin/gg/destiny/lizard/base/extensions/NumberExtensions.kt
+++ app/src/main/kotlin/gg/destiny/lizard/base/extensions/NumberExtensions.kt
@@ -3,6 +3,8 @@ package gg.destiny.lizard.base.extensions
 import android.content.Context
 import android.util.TypedValue
 
-fun Int.dp(context: Context) =
+annotation class Dp
+
+fun @receiver:Dp Int.dp(context: Context) =
   Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics))
 
