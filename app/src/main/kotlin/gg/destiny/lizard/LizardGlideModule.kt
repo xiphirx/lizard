2e351bb512660097fb19090dd2a5e691060a6f5a
Hilal Alsibai <xiphirx@users.noreply.github.com>
2018-10-28 02:25:05 -0700

    Update Glide
    Wow new version

---
+++ app/src/main/kotlin/gg/destiny/lizard/LizardGlideModule.kt
@@ -0,0 +1,7 @@
+package gg.destiny.lizard
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class LizardGlideModule : AppGlideModule()
